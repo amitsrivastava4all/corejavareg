81b851939f9713a4ee3b7e8da30de6cf38f841d9
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-06-13 13:53:03 +0530

    Code

---
+++ DataTypes.java
@@ -0,0 +1,38 @@
+strictfp class DataTypes{
+    
+    public  static void main(String[] args) {
+        boolean x = true;
+        String b = "नमस्कार जावा";
+        System.out.println(b);
+        char a = 'A'; // UNICODE
+        long q;
+        float e = 90.20f;
+        double tt= 90.20;
+        
+        
+        
+        if(10>2){
+            q = 10000L;
+        }
+        q++;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+    }
+}
---
+++ Demo.java
@@ -0,0 +1,16 @@
+class Demo{
+   public static void  main(String arr[]){
+       int x1= 90;
+       //int firstnumber = 100;
+       // int first_number = 100;
+       int firstNumber =10;
+       int secondNumber =20;
+       int sum = firstNumber + secondNumber;
+       System.out.print("First IS \t"+firstNumber+"\n AND Second is "+secondNumber);
+       System.out.println("Sum is "+sum);
+
+
+
+    }
+
+}
