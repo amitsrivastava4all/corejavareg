b70a968745696997c41f35de14888a01a155d188
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-06-15 14:04:03 +0530

    Code

--- Demo.java
+++ Demo.java
@@ -1,16 +1,21 @@
-class Demo{
-   public static void  main(String arr[]){
-       int x1= 90;
-       //int firstnumber = 100;
-       // int first_number = 100;
-       int firstNumber =10;
-       int secondNumber =20;
-       int sum = firstNumber + secondNumber;
-       System.out.print("First IS \t"+firstNumber+"\n AND Second is "+secondNumber);
+class Add{
+    //static int count;
+    public static void main(int x){
+        System.out.println("My Main Method");
+    }
+   static public  void main(String args[]){
+       int sum = 0;
+       for(int i = 0; i<args.length; i++){
+        sum+=Integer.parseInt(args[i]);
+       }
        System.out.println("Sum is "+sum);
-
-
-
+      /* if(args.length==2){
+    int a=Integer.parseInt(args[0]);
+    int b=Integer.parseInt(args[1]);
+    int c = a + b;
+   // main(10);    
+    System.out.println("Sum is "+c);
+       }*/
     }
 
 }
