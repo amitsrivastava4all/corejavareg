f4f01dbfcad0c1f768de9ea47b5500a1c8333788
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-06-16 14:06:26 +0530

    Code

--- DataTypes.java
+++ DataTypes.java
@@ -1,40 +1,56 @@
-strictfp class DataTypes{
-    void show(){
-        float w = 90.10f;
+/*class BO{
+    void recData(User user){
+
     }
-    public  static void main(String[] args) {
-        boolean x = true;
-        String b = "नमस्कार जावा";
-        System.out.println(b);
-        char a = 'A'; // UNICODE
-        long q;
-        float e = 90.20F;
-        double tt= 90.20;
+    void recData(int id , char name[], char gender, 
+    char[] city, char []phone, course, duration , mail,){
+
+    }
+}
+class User{
+    id, name, gender, city, phone, 
+}*/
+import java.math.BigDecimal;
+import java.math.BigInteger;
+strictfp class DataTypes{
+    public  static void main(String args[]){
         
+        int k1 = 1000;
+        String.valueOf(k1);
+        short s2 = (short) k1;
+        double tt  = k1;
+        String a = "Ram";
+        Integer s = 1000;
+        byte qq = s.byteValue();  // xxxValue()
+        double w1 = s.doubleValue();
+        Strinng h1  = s.toString();
+        BigDecimal bb = new BigDecimal("1244548843.992");
+        String name = "Amit";
+        BigInteger bg = new BigInteger("1000000000");
+        BigInteger bg2 = new BigInteger("12000000000");
+        BigInteger bg3  = bg.add(bg2);
+        System.out.println("Sum is "+bg3);
         
-        
-        if(10>2){
-            q = 10000L;
-        }
-        q++;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-       
+        boolean flag = true; //1 , 2 , 4 byte
+        char w4 = 'A'; // 2 Byte
+        long w11 = 10000L; // 8 Byte
+        float w2 = 90.20F; // 4 Byte
+        double w3 = 90.12;  // 8 Byte
+
+        short q = 100; //2 byte
+        int w = 100000; // 4 byte
+        byte x = 10; // 1 Byte 
+        x = (byte)130;
+        System.out.println("X is "+x);
+        byte a = 100;
+        byte b = 100;
+        b+=a; // Arithmetic Assignment Operator
+        // += -= *= /=
+        //b =(byte) (b + a);
+        byte c = (byte)(a+b);
+        //byte c = a + b;
+        System.out.println("Sum is "+b);
+
+
     }
 }
---
+++ Pool.java
@@ -0,0 +1,16 @@
+class Pool{
+    public static void main(String[] args) {
+        Character c = 'A';
+        
+        String name = "Amit";
+        name.charAt(index)
+        String name2 ="Amit";
+        String name3 = new String("Amit");
+        System.out.println(name==name2);
+        System.out.println(name==name3);
+        Integer q = 128;
+        Integer q1 = 128;
+        System.out.println(q==q1);
+
+    }
+}
